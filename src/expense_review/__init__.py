6c29e854887cf2cf1313a13e6d29100c1ca9ad0b
oucan <gzcti@qq.com>
2026-06-10 05:45:45 +0800

    feat: 创建 expense_review 包结构

---
+++ src/expense_review/__init__.py
@@ -0,0 +1,5 @@
+from .reviewer import ExpenseReviewer
+from .reporter import ReportGenerator
+from .notifier import WebhookNotifier
+
+__all__ = ["ExpenseReviewer", "ReportGenerator", "WebhookNotifier"]
---
+++ src/expense_review/notifier.py
@@ -0,0 +1,2 @@
+class WebhookNotifier:
+    pass
